33613e65695ade3596bdafb910dd2d034d67ccb2
nishat <nishatlabiba98@gmail.com>
2026-03-19 15:57:57 -0400

    added cli

---
+++ cradlenotes-api/src/main/java/com/cradlenotes/cradlenotes/CradlenotesApplication.java
@@ -0,0 +1,13 @@
+package com.cradlenotes.cradlenotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CradlenotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CradlenotesApplication.class, args);
+	}
+
+}
---
+++ cradlenotes-api/src/main/java/com/cradlenotes/cradlenotes/Notescontroller.java
@@ -0,0 +1,16 @@
+
+package com.cradlenotes;
+ 
+import org.springframework.web.bind.annotation.*;
+import java.io.IOException;
+import java.nio.file.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Stream;
+
+
+
+public class Notescontroller {
+    
+}
